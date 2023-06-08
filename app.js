774433172ae307d7ce3e8059db3906b9e8483a7a
upesh-upreti <upeshupreti@gmail.com>
2023-06-08 15:10:14 +0545

    adds cors with origin *.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 require('./config/dbConnect');
+const cors = require('cors');
 
 const app = express();
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Upesh Upreti</h1>')
