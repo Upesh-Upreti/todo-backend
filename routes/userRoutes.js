6d788154a52c6f7a4a58273ce637ef4eefa7849b
upesh-upreti <upeshupreti@gmail.com>
2023-07-03 15:18:13 +0545

    adds change password feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const userControllers = require('../controllers/userController');
 const userSchema = require('../schemas/userSchema');
 const validateRequestSchema = require('../middlewares/validateRequestSchema');
+const validateUser = require('../middlewares/validateUser');
 
 router.post('/sign-up', userSchema.signUpSchema, validateRequestSchema, userControllers.signUp);
 
-router.post('/log-in', userSchema.logInSchema, validateRequestSchema, userControllers.logIn);
+router.post('/login', userSchema.logInSchema, validateRequestSchema, userControllers.logIn);
+
+router.post('/change-password', validateUser, userSchema.changePasswordSchema, validateRequestSchema, userControllers.changePassword);
+
 
 module.exports = router;
